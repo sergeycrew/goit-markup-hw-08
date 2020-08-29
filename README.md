5382a75f19ac1e9e8884be3857e42b123a3a47de
sergeycrew <67499357+sergeycrew@users.noreply.github.com>
2020-08-29 23:23:04 +0300

    8.6 final cut

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# goit-markup-hw-07
-HW7
+# goit-markup-hw-08
--- js/modal.js
+++ js/modal.js
@@ -1,17 +1,15 @@
 (() => {
   const refs = {
-    openModalBtn: [...document.querySelectorAll("[data-modal-open]")],
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
   };
 
-  refs.openModalBtn.forEach((e) => {
-    e.addEventListener("click", toggleModal);
-  });
-
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
+    document.body.classList.toggle('modal-open');
+    refs.modal.classList.toggle('is-hidden');
   }
 })();
